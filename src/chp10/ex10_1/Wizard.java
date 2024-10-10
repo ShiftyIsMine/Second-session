5853ddbc0f1a928b84cfcd01350331c878b356ae
Shifty <jjs5827852@naver.com>
2024-10-10 11:34:12 +0900

    1010Java

---
+++ src/chp10/ex10_1/Wizard.java
@@ -0,0 +1,8 @@
+package chp10.ex10_1;
+
+public class Wizard extends Novice {
+    int mp;
+    void fireB(){
+        System.out.printf("%s(HP : %d, MP : %d)의 파이어 볼 ~~~§",name,hp,mp);
+    }
+}
---
+++ src/java1010/arrayListTest2.java
@@ -0,0 +1,10 @@
+package java1010;
+
+import java.util.ArrayList;
+
+public class arrayListTest2 {
+    public static void main(String[] args) {
+        ArrayList<String> items = new ArrayList<String>();
+
+    }
+}
