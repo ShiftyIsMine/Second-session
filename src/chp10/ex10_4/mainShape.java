f045ca2edc5118a77345ff9e2f18572cc2e479c3
Shifty <jjs5827852@naver.com>
2024-10-17 09:45:32 +0900

    1017 JAVA

--- src/chp10/ex10_4/mainShape.java
+++ src/chp10/ex10_4/mainShape.java
@@ -1,5 +1,5 @@
 package chp10.ex10_4;
-
+import java.util.ArrayList;
 public class mainShape {
     public static void main(String[] args) {
         square s = new square();
@@ -8,12 +8,20 @@ public class mainShape {
         t.name = "삼각형";
         circle c = new circle();
         c.name = "원";
-
-//        shape s1 = new square();//이것두 참조를 부모 클래스로 했으면 그것도 업캐스팅
-        shape[] shapes = {s,t,c};//업캐스팅 but 자식 클래스에 마음대로 접근 불가능
-        for(int i=0;i<shapes.length;i++){
-            System.out.printf("인덱스 번호 %d번의 도형 이름 : %s\n",i,shapes[i].name);
+//
+////        shape s1 = new square();//이것두 참조를 부모 클래스로 했으면 그것도 업캐스팅
+//        shape[] shapes = {s,t,c};//업캐스팅 but 자식 클래스에 마음대로 접근 불가능
+//        for(int i=0;i<shapes.length;i++){
+//            System.out.printf("인덱스 번호 %d번의 도형 이름 : %s\n",i,shapes[i].name);
+//        }
+////        (square)shapes[i]>>> 다운 캐스팅 but 부모 클래스의 변수를 사용 못 함
+        ArrayList<shape> shapes = new ArrayList<shape>();
+        shapes.add(s);
+        shapes.add(t);
+        shapes.add(c);
+        for(int i=0;i<shapes.size();i++){
+            System.out.printf("인덱스 번호 %d번의 도형 이름 : %s\n",i,shapes.get(i).name);
         }
-//        (square)shapes[i]>>> 다운 캐스팅 but 부모 클래스의 변수를 사용 못 함
+
     }
 }
