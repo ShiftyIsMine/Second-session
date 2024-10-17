0a5f0aa61c25343181d383a8364e0ce2b152e53c
Shifty <jjs5827852@naver.com>
2024-10-17 09:52:37 +0900

    1017 JAVA3

---
+++ src/chp10/ex10_4/mainSqaure.java
@@ -0,0 +1,9 @@
+package chp10.ex10_4;
+
+public class mainSqaure {
+    public static void main(String[] args) {
+        square squ = new square();
+
+        shape shapes = new square();
+    }
+}
